0968a23c7db7a919158727c3f7102db6c7d8c289
WIN-20240425NTW <616481584@qq.com>
2024-08-07 10:57:58 +0800

    登陆模块，用户模块和地址模块

---
+++ src/pagesMember/address/address.vue
@@ -0,0 +1,227 @@
+<script setup lang="ts">
+import { deleteMemberAddressByIdAPI, getMemberAddressAPI } from '@/services/address'
+import { useAddressStore } from '@/stores/modules/address'
+import type { AddressItem } from '@/types/address'
+import { onShow } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+
+// 获取收货地址列表数据
+const addressList = ref<(AddressItem & { show: boolean })[]>([])
+const getMemberAddressData = async () => {
+  const res = await getMemberAddressAPI()
+  let tempList = res.result.map((item) => {
+    return {
+      show: false,
+      ...item,
+    }
+  })
+  addressList.value = tempList
+}
+
+// 初始化调用(页面显示)
+onShow(() => {
+  getMemberAddressData()
+})
+
+// 修改收货地址
+const onChangeAddress = (item: AddressItem) => {
+  // 修改地址
+  const addressStore = useAddressStore()
+  addressStore.changeSelectedAddress(item)
+  // 返回上一页
+  uni.navigateBack()
+}
+
+//
+const swipeActionOption = [
+  {
+    text: '修改',
+    style: {
+      backgroundColor: '#27ba9b',
+    },
+  },
+  {
+    text: '删除',
+    style: {
+      backgroundColor: '#dd524d',
+    },
+  },
+]
+const openSwipeAction = (index: number) => {
+  // 先将正在被操作的swipeAction标记为打开状态，否则由于props的特性限制，
+  // 原本为'false'，再次设置为'false'会无效
+  addressList.value[index].show = true
+  addressList.value.map((val, idx) => {
+    if (index != idx) addressList.value[idx].show = false
+  })
+}
+const onClickSwipeAction = (index: number, index1: number) => {
+  if (index1 == 0) {
+    // 修改
+    let id = addressList.value[index].id
+    uni.navigateTo({
+      url: '/pagesMember/address-form/address-form?id=' + id,
+    })
+  }
+  if (index1 == 1) {
+    // 删除
+    let id = addressList.value[index].id
+    uni.showModal({
+      content: '删除地址?',
+      confirmColor: '#27BA9B',
+      success: async (res) => {
+        if (res.confirm) {
+          // 根据id删除收货地址
+          await deleteMemberAddressByIdAPI(id)
+          // 重新获取收货地址列表
+          getMemberAddressData()
+        }
+      },
+    })
+  }
+}
+</script>
+
+<template>
+  <view class="viewport">
+    <!-- 地址列表 -->
+    <scroll-view enable-back-to-top class="scroll-view" scroll-y>
+      <view v-if="addressList.length" class="address">
+        <view class="address-list">
+          <!-- 收货地址项 -->
+          <u-swipe-action
+            v-for="(item, index) in addressList"
+            :disabled="false"
+            :index="index"
+            :show="item.show"
+            :key="item.id"
+            :options="swipeActionOption"
+            @click="onClickSwipeAction"
+            @open="openSwipeAction(index)"
+          >
+            <view class="item-content" @tap="onChangeAddress(item)">
+              <view class="user">
+                {{ item.receiver }}
+                <text class="contact">{{ item.contact }}</text>
+                <text v-if="item.isDefault" class="badge">默认</text>
+              </view>
+              <view class="locate">{{ item.fullLocation }} {{ item.address }}</view>
+            </view>
+          </u-swipe-action>
+        </view>
+      </view>
+      <view v-else class="blank">暂无收货地址</view>
+    </scroll-view>
+    <!-- 添加按钮 -->
+    <view class="add-btn">
+      <navigator hover-class="none" url="/pagesMember/address-form/address-form">
+        新建地址
+      </navigator>
+    </view>
+  </view>
+</template>
+
+<style lang="scss">
+page {
+  height: 100%;
+  overflow: hidden;
+}
+
+/* 删除按钮 */
+.delete-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  height: 100%;
+  font-size: 28rpx;
+  color: #fff;
+  border-radius: 0;
+  padding: 0;
+  background-color: #cf4444;
+}
+
+.viewport {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  background-color: #f4f4f4;
+
+  .scroll-view {
+    padding-top: 20rpx;
+  }
+}
+
+.address {
+  padding: 0 20rpx;
+  margin: 0 20rpx;
+  border-radius: 10rpx;
+  background-color: #fff;
+
+  .item-content {
+    line-height: 1;
+    padding: 40rpx 10rpx 38rpx;
+    border-bottom: 1rpx solid #ddd;
+    position: relative;
+
+    .edit {
+      position: absolute;
+      top: 36rpx;
+      right: 30rpx;
+      padding: 2rpx 0 2rpx 20rpx;
+      border-left: 1rpx solid #666;
+      font-size: 26rpx;
+      color: #666;
+      line-height: 1;
+    }
+  }
+
+  .item:last-child .item-content {
+    border: none;
+  }
+
+  .user {
+    font-size: 28rpx;
+    margin-bottom: 20rpx;
+    color: #333;
+
+    .contact {
+      color: #666;
+    }
+
+    .badge {
+      display: inline-block;
+      padding: 4rpx 10rpx 2rpx 14rpx;
+      margin: 2rpx 0 0 10rpx;
+      font-size: 26rpx;
+      color: #27ba9b;
+      border-radius: 6rpx;
+      border: 1rpx solid #27ba9b;
+    }
+  }
+
+  .locate {
+    line-height: 1.6;
+    font-size: 26rpx;
+    color: #333;
+  }
+}
+
+.blank {
+  margin-top: 300rpx;
+  text-align: center;
+  font-size: 32rpx;
+  color: #888;
+}
+
+.add-btn {
+  height: 80rpx;
+  text-align: center;
+  line-height: 80rpx;
+  margin: 30rpx 20rpx;
+  color: #fff;
+  border-radius: 80rpx;
+  font-size: 30rpx;
+  background-color: #27ba9b;
+}
+</style>
